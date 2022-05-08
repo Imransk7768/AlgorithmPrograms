92ccf021cbab81641d647bfa07e6f687556952f7
Imransk7768 <imransk7768@gmail.com>
2022-05-09 01:32:49 +0530

    [Imran] Added : InsertionSort of String Value

--- AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/Program.cs
@@ -8,7 +8,7 @@ namespace AlgorithmPrograms
         {
             Console.WriteLine(">>>>  Welcome to Algorithm Programs  <<<<");
             bool end = true;
-            Console.WriteLine("1. Binary Search\n   Otherthan Above Option(s) Program Ends");
+            Console.WriteLine("1. Binary Search\n2. InsertionSort\n   Otherthan Above Option(s) Program Ends");
             while (end == true)
             {
                 Console.Write("Take an option to execute : ");
@@ -18,6 +18,13 @@ namespace AlgorithmPrograms
                     case 1:
                         BinarySearch binarySearch = new BinarySearch();
                         binarySearch.Binary();
+                        break;
+                    case 2:
+                        InsertionSort sort = new InsertionSort();
+                        const string filePath = @"H:\Assignments\AlgorithmPrograms\AlgorithmPrograms\ProgramData.txt";
+                        string word = File.ReadAllText(filePath);
+                        string[] names = word.Split(',');
+                        sort.Insertion(names);
                         break;
                     default:
                         end = false;
