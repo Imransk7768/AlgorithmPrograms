9ad4a07bb52aac6f1076a84dd80f9dd9e6587a45
Imransk7768 <imransk7768@gmail.com>
2022-05-08 23:32:43 +0530

    [Imran] Added : Displays Welcome

---
+++ AlgorithmPrograms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlgorithmPrograms
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(">>>>  Welcome to Algorithm Programs  <<<<");
+        }
+    }
+}
