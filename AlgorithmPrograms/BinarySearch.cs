39f7864b21ae8d021364fa8a2ad58b85379a1d44
Imransk7768 <imransk7768@gmail.com>
2022-05-09 01:02:16 +0530

    [Imran] Added : Binary String Value Search

---
+++ AlgorithmPrograms/BinarySearch.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AlgorithmPrograms
+{
+    public class BinarySearch
+    {
+        public void Binary()
+        {
+            bool search = false;
+            const string filepath = @"H:\Assignments\AlgorithmPrograms\AlgorithmPrograms\ProgramData.txt";
+            string list = File.ReadAllText(filepath);
+            string[] arr = list.Split(" ");
+            Console.WriteLine("Enter The Word");
+            string name = Console.ReadLine();
+            foreach (string word in arr)
+            {
+                if (word.Equals(name))
+                {
+                    search = true;
+                    Console.WriteLine("'" + name + "' Word Is Present/Exist.");
+                }
+            }
+            if (search == false)
+            {
+                Console.WriteLine("Word '" + name + "' Is Not Present/Exist.");
+            }
+        }
+    }
+}
--- AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/Program.cs
@@ -7,6 +7,24 @@ namespace AlgorithmPrograms
         public static void Main(string[] args)
         {
             Console.WriteLine(">>>>  Welcome to Algorithm Programs  <<<<");
+            bool end = true;
+            Console.WriteLine("1. Binary Search\n   Otherthan Above Option(s) Program Ends");
+            while (end == true)
+            {
+                Console.Write("Take an option to execute : ");
+                int check = Convert.ToInt32(Console.ReadLine());
+                switch (check)
+                {
+                    case 1:
+                        BinarySearch binarySearch = new BinarySearch();
+                        binarySearch.Binary();
+                        break;
+                    default:
+                        end = false;
+                        Console.WriteLine("Program Ends.");
+                        break;
+                }
+            }
         }
     }
 }
