89641e9516cb9b342468541edf6add88140e3f22
Imransk7768 <imransk7768@gmail.com>
2022-05-09 17:58:04 +0530

    [Imran] Added: Implement MergeSort to array

---
+++ AlgorithmPrograms/MergeSort.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    public class MergeSort
+    {
+        public void Merge(int[] arr2, int l, int m, int r)
+        {
+            int a = m - l + 1;
+            int b = r - m;
+
+            int[] L = new int[a];
+            int[] R = new int[b];
+            int i, j;
+
+            for (i = 0; i < a; ++i)
+                L[i] = arr2[l + i];
+            for (j = 0; j < b; ++j)
+                R[j] = arr2[m + 1 + j];
+            i = 0;
+            j = 0;
+            int k = l;
+            while (i < a && j < b)
+            {
+                if (L[i] <= R[j])
+                {
+                    arr2[k] = L[i];
+                    i++;
+                }
+                else
+                {
+                    arr2[k] = R[j];
+                    j++;
+                }
+                k++;
+            }
+            while (i < a)
+            {
+                arr2[k] = L[i];
+                i++;
+                k++;
+            }
+            while (j < b)
+            {
+                arr2[k] = R[j];
+                j++;
+                k++;
+            }
+        }
+        public void Sorting(int[] arr2, int l, int r)
+        {
+            if (l < r)
+            {
+                int m = l + (r - l) / 2;
+                Sorting(arr2, l, m);
+                Sorting(arr2, m + 1, r);
+                Merge(arr2, l, m, r);
+            }
+        }
+        public void PrintArray(int[] arr2)
+        {
+            int n = arr2.Length;
+            for (int i = 0; i < n; ++i)
+                Console.Write(arr2[i] + " ");
+            Console.WriteLine();
+        }
+    }
+}
--- AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/Program.cs
@@ -8,7 +8,7 @@ namespace AlgorithmPrograms
         {
             Console.WriteLine(">>>>  Welcome to Algorithm Programs  <<<<");
             bool end = true;
-            Console.WriteLine("1. Binary Search\n2. InsertionSort\n3. Bubble Sort\n   Otherthan Above Option(s) Program Ends");
+            Console.WriteLine("1. Binary Search\n2. InsertionSort\n3. Bubble Sort\n4. Merge SOrt\n   Otherthan Above Option(s) Program Ends");
             while (end == true)
             {
                 Console.Write("Take an option to execute : ");
@@ -29,6 +29,15 @@ namespace AlgorithmPrograms
                     case 3:
                         BubbleSort bubble = new BubbleSort();
                         bubble.BubbleSorting();
+                        break;
+                    case 4:
+                        MergeSort merge = new MergeSort();
+                        int[] arr2 = { 22, 12, 15, 7, 6, 5};
+                        Console.Write("Array Values : ");
+                        merge.PrintArray(arr2);
+                        merge.Sorting(arr2, 0, arr2.Length - 1);
+                        Console.Write("Sorted array : ");
+                        merge.PrintArray(arr2);
                         break;
                     default:
                         end = false;
